aca4427d2269ab3f2a6b4324e2d9079bbb9c7287
Leah <jiaxint@mtu.edu>
2020-11-09 15:54:15 -0600

    callback files add

---
+++ resources/callBack.js
@@ -0,0 +1,47 @@
+const { rejects } = require("assert");
+const { promises } = require("fs");
+const { resolve } = require("path");
+
+console.log("before ");
+// getUser(1, (user)=>{
+//     console.log('user', user);
+
+//     //get repo
+//     getRepo(user.gitHubUserName)
+// })
+
+function getUser(id, callback){
+    setTimeout(()=>{
+        console.log('reading user from database');
+        callback({id: id, gitHubUserName: "jia"});
+    }, 2000)
+}
+
+function getRepo(username, callback){
+    setTimeout(()=>{
+        console.log("reading dta from github");
+        callback(['repo1', 'repo2'])
+    }, 2000)
+}
+
+//promise
+function getProName(id){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log("reading data");
+            resolve({id: id, getUserName: 'jia'})
+        }, 2000)
+    })
+}
+function getProRepo(name){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log("getting repo");
+            resolve(['repo1', 'repo2'])
+        }, 2000)
+    })
+}
+
+getProName(1).then(user=>getProRepo(user.getUserName))
+.then(repo=>console.log(repo))
+.catch(error=>console.error("error catch: ", error.message))
